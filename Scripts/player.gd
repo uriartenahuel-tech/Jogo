23d2bf4c8c0c0c0bc2f6aea80d8b1d4d0a7e4286
Nahuel <uriarte.nahuelalejandro@gmail.com>
2025-12-13 12:21:55 -0300

    8-way movement

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,45 +1,9 @@
 extends CharacterBody2D
 
-var speed = 2500
-var direction = "right"
-
-
+@export var speed : float = 50
+var input
 
 func _physics_process(delta: float) -> void:
-	if Input.is_action_pressed("ui_right"):
-		direction = "right"
-		player_animation(1)
-		velocity.x = speed * delta
-		velocity.y = 0
-	elif Input.is_action_pressed("ui_left"):
-		direction = "left"
-		player_animation(1)
-		velocity.x = -speed * delta
-		velocity.y = 0
-	elif Input.is_action_pressed("ui_up"):
-		player_animation(1)
-		velocity.x = 0
-		velocity.y = -speed * delta
-	elif Input.is_action_pressed("ui_down"):
-		player_animation(1)
-		velocity.x = 0
-		velocity.y = speed * delta
-	else:
-		player_animation(0)
-		velocity.x = 0
-		velocity.y = 0
+	var input = Input.get_vector("left", "right", "up", "down")
+	velocity = input * speed
 	move_and_slide()
-
-
-func player_animation(movement):
-	
-	if direction == "right" :
-		$AnimatedSprite2D.flip_h = false
-	elif direction == "left":
-		$AnimatedSprite2D.flip_h = true
-		
-	if movement == 1:
-		$AnimatedSprite2D.play("walk")
-	else:
-		$AnimatedSprite2D.play("idle")
-		
--- Scripts/proyectil.gd
+++ Scripts/proyectil.gd
@@ -2,12 +2,19 @@ extends RigidBody2D
 
 @export var speed := 100.0
 @export var rotation_speed := 10.0 # radians/segundo (probá 5, 10, 20)
+@export var visibleOnScreen : VisibleOnScreenNotifier2D
+
 
 var direction := Vector2.ZERO
 
 func _ready():
 	gravity_scale = 0
-	angular_velocity = rotation_speed
+	angular_velocity = -rotation_speed
 
 func _physics_process(_delta):
 	linear_velocity = direction * speed
+	visible()
+
+func visible():
+	if not visibleOnScreen.is_on_screen():
+		queue_free()
